3f90b18cdcd10a668cf05ee9c4c846eaa5795c68
Daniel Mak <danielmakcy@gmail.com>
2023-09-11 16:42:46 -0400

    Update at 4:42

--- SQL/Query 1.sql
+++ SQL/Query 1.sql
@@ -1,27 +1,24 @@
 -- Query 1 - Performance
--- Use Case: Compare monthly revenue (per active customer) of each store
+-- Use Case: Compare revenue (per active customer) from each city
 
-WITH Monthly_Revenue AS (
-SELECT extract (year from rental_date) as Year, extract (month from rental_date) as Month, i.store_id, r.customer_id, sum(p.amount) as revenue FROM RENTAL r 
+WITH Revenue AS (
+SELECT cy.city, sum(p.amount) as revenue FROM RENTAL r 
 JOIN PAYMENT p on p.rental_id = r.rental_id
-JOIN Inventory i on i.inventory_id = r.inventory_id
-Group by extract (year from rental_date) , extract (month from rental_date), i.store_id, r.customer_id
-Order by extract (year from rental_date) , extract (month from rental_date)
+JOIN CUSTOMER c on c.customer_id = r.customer_id
+JOIN ADDRESS a on a.address_id = c.address_id
+JOIN CITY cy on cy.city_id = a.city_id
+Group by cy.city
+ORDER by sum(p.amount) DESC 
 ),
 
 Active_Customer AS (
-SELECT c.store_id, count(1) as Total_Active_Cust from customer c
+SELECT ct.city, count(1) as Total_Active_Cust from customer c
+JOIN ADDRESS a on a.address_id = c.address_id
+JOIN CITY ct on ct.city_id = a.city_id
 WHERE c.active = 1 
-GROUP by c.store_id
+GROUP by ct.city
 )
 
-
-SELECT r.store_id, r.year, r.month, sum(r.revenue / c.total_active_cust) from Monthly_Revenue r
-JOIN Active_Customer c on c.store_id = r.store_id
-GROUP by r.year, r.month, r.store_id;
-
-
---SELECT * from Active_Customer
-
---SELECT r.store_id, r.year, r.month, sum(r.revenue) from Monthly_Revenue r
---GROUP by r.year, r.month, r.store_id; 
+SELECT r.city, r.revenue as Total_Revenue,  r.revenue/ac.total_active_cust as Revenue_per_Cust from Revenue r
+JOIN Active_Customer ac on ac.city = r.city
+Order by r.revenue DESC;
--- SQL/Query 4.sql
+++ SQL/Query 4.sql
@@ -1,20 +1,17 @@
 --Query 4  - Promotion 
 -- Use Case : Identify list of customer that have lower than average rental in the past period.
 
-WITH Monthly_Revenue AS (
-SELECT i.store_id, r.customer_id, sum(p.amount) as revenue FROM RENTAL r 
+WITH Revenue AS (
+SELECT r.customer_id, sum(p.amount) as revenue FROM RENTAL r 
 JOIN PAYMENT p on p.rental_id = r.rental_id
 JOIN Inventory i on i.inventory_id = r.inventory_id
-Group by i.store_id, r.customer_id
-),
-
-Average_Rental AS (
-SELECT Store_id, avg(revenue) as avg_revenue from Monthly_Revenue
-Group by 1
+Group by r.customer_id
+Order by 1
 )
 
-SELECT c.store_id, first_name, last_name, email, mr.revenue, ar.avg_revenue from customer c 
-JOIN Average_Rental ar on ar.store_id = c.store_id
-JOIN Monthly_Revenue mr on mr.customer_id = c.customer_id
-WHERE mr.revenue < ar.avg_revenue
-order by c.store_id;
+SELECT ct.city, first_name, last_name, email, r.revenue from customer c
+JOIN Revenue r on r.customer_id = c.customer_id
+JOIN Address a on a.address_id = c.address_id
+JOIN City ct on ct.city_id = a.address_id
+WHERE r.revenue < (SELECT avg(revenue) from Revenue)
+order by ct.city;
